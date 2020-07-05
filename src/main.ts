016ca9a1d13f2f74343116c3cf7ca848bcdd3942
Scott Prue <sprue.dev@gmail.com>
2020-07-05 14:28:31 -0400

    Switch to calling firebase bin path with node in front

--- src/main.ts
+++ src/main.ts
@@ -228,8 +228,8 @@ export default async function run(): Promise<void> {
         // Call deploy command with listener for output (so that in case of failure,
         // it can be parsed for a list of functions which must be re-deployed)
         const deployExitCode = await exec(
-          firebaseCommand,
-          [...deployArgs, '--project', projectId],
+          nodePath,
+          [firebaseCommand, ...deployArgs, '--project', projectId],
           {
             listeners: {
               stdout: (data: Buffer) => {
@@ -258,8 +258,8 @@ export default async function run(): Promise<void> {
             const newDeployCommand = searchResults && searchResults[1];
             let secondDeployOutput = '';
             const secondDeployExitCode = await exec(
-              firebaseCommand,
-              [...(newDeployCommand?.split(' ') || [])],
+              nodePath,
+              [firebaseCommand, ...(newDeployCommand?.split(' ') || [])],
               {
                 listeners: {
                   stdout: (data: Buffer) => {
